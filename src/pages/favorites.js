8fa2f066f85a84a4edb6c1b1f32087857f6e2385
Julien Aubert <julien.aubert.rbs@gmail.com>
2021-02-10 11:09:20 +0100

    favorite page

--- src/pages/favorites.js
+++ src/pages/favorites.js
@@ -1,11 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import '../App.css';
+// Components
+import CharacterList from '../components/CharacterList';
 
-const favorites = () => {
+const Favorites = () => {
+  const [favCharacters, setFavCharacters] = useState(null);
+  useEffect(async () => {
+    await axios
+      .get('/favorites')
+      .then((results) => {
+        setFavCharacters(results.data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+  let favCharacterMarkup = favCharacters ? (
+    favCharacters.map((character) => (
+      <CharacterList key={character.id} character={character} />
+    ))
+  ) : (
+    <p>loading...</p>
+  );
   return (
     <div>
       <h1>Favorites page</h1>
+      <ul className="character-list">{favCharacterMarkup}</ul>
     </div>
   );
 };
 
-export default favorites;
+export default Favorites;
